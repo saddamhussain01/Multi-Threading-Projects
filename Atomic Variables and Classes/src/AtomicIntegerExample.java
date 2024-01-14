844528a175c75524b9677431b721219db8faf48c
saddamhussain01 <saddam998929@gmail.com>
2024-01-14 14:38:47 +0530

    added project

---
+++ Atomic Variables and Classes/src/AtomicIntegerExample.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerExample {
+	
+    public static void main(String[] args) {
+    
+    	AtomicInteger atomicInt = new AtomicInteger(0);
+
+        Runnable incrementTask = () -> {
+        	
+            int newValue = atomicInt.incrementAndGet();
+            
+            System.out.println("New value: " + newValue);
+            
+        };
+
+        for (int i = 0; i < 5; i++) {
+        	
+            new Thread(incrementTask).start();
+        }
+    }
+}
